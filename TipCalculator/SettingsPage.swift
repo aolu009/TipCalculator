abdc84327da1cb95f7f0b851010d72d9f1b3e4cf
aolu009 <aolu009@gmail.com>
2016-08-01 03:48:11 -0700

    Initial Commit

---
+++ TipCalculator/SettingsPage.swift
@@ -0,0 +1,98 @@
+//
+//  SettingsPage.swift
+//  TipCalculator
+//
+//  Created by Lu Ao on 7/26/16.
+//  Copyright © 2016 Lu Ao. All rights reserved.
+//
+
+import UIKit
+
+class SettingsPage: UIViewController {
+
+    
+    @IBOutlet weak var tipTier1: UITextField!
+    @IBOutlet weak var tipTier2: UITextField!
+    @IBOutlet weak var tipTier3: UITextField!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    
+    var tipPercentages = [Double]()
+    var tipControl: UISegmentedControl!
+    var themeGreen: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(tipPercentages)
+        //print(tipControl.titleForSegmentAtIndex(0))
+        tipTier1.text = String(tipPercentages[0]*100)
+        tipTier2.text = String(tipPercentages[1]*100)
+        tipTier3.text = String(tipPercentages[2]*100)
+        tipTier1.layer.borderColor = UIColor.blueColor().CGColor
+        tipTier1.layer.borderWidth = CGFloat(1.0)
+        tipTier2.layer.borderColor = UIColor.blueColor().CGColor
+        tipTier2.layer.borderWidth = CGFloat(1.0)
+        tipTier3.layer.borderColor = UIColor.blueColor().CGColor
+        tipTier3.layer.borderWidth = CGFloat(1.0)
+        themeSwitch.on = defaults.boolForKey("themeSwitch")
+        themeGreen = defaults.boolForKey("themeGreen")
+    }
+    
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    @IBAction func changeTier1(sender: AnyObject) {
+        let newTip: Double = (tipTier1.text! as NSString).doubleValue/100
+        tipPercentages[0] = newTip
+    }
+    
+    @IBAction func changeTier2(sender: AnyObject) {
+        let newTip: Double = (tipTier2.text! as NSString).doubleValue
+        tipPercentages[1] = newTip/100
+    }
+    
+    @IBAction func changeTier3(sender: AnyObject) {
+        let newTip: Double = (tipTier3.text! as NSString).doubleValue
+        tipPercentages[2] = newTip/100
+    
+    }
+    
+    @IBAction func greenTheme(sender: UISwitch) {
+        if themeSwitch.on == true{
+            themeGreen = true
+            defaults.setBool(themeSwitch.on, forKey: "themeSwitch")
+            defaults.setBool(themeGreen, forKey: "themeGreen")
+            defaults.synchronize()
+        }
+        if themeSwitch.on == false{
+            themeGreen = false
+            defaults.setBool(themeSwitch.on, forKey: "themeSwitch")
+            defaults.setBool(themeGreen, forKey: "themeGreen")
+            defaults.synchronize()
+        }
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "Back") {
+            
+            let navController = segue.destinationViewController as! UINavigationController
+            let destinationVC = navController.topViewController as! MainPage
+            
+            destinationVC.tipPercentages = tipPercentages
+            destinationVC.tipControl = tipControl
+            destinationVC.themeGreen = themeGreen
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.performSegueWithIdentifier("Back" , sender: tipPercentages)
+    }
+}
---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Lu Ao on 7/24/16.
+//  Copyright © 2016 Lu Ao. All rights reserved.
+//
+
+import UIKit
+
+class MainPage: UIViewController {
+
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabek: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var taxAmount: UITextField!
+    @IBOutlet weak var taxcalculated: UILabel!
+    
+    
+       override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let locale = NSLocale.currentLocale()
+        let currencySymbol = locale.objectForKey(NSLocaleCurrencySymbol) as? String
+        print("",currencySymbol!)
+        tipLabel.text = currencySymbol!+"0.00"
+        totalLabek.text = currencySymbol!+"0.00"
+        //taxcalculated.text = currencySymbol!+"0.00"
+        billField.becomeFirstResponder()
+        tipControl.setTitle(String(tipPercentages[0]*100)+"%", forSegmentAtIndex: 0)
+        tipControl.setTitle(String(tipPercentages[1]*100)+"%", forSegmentAtIndex: 1)
+        tipControl.setTitle(String(tipPercentages[2]*100)+"%", forSegmentAtIndex: 2)
+        billField.layer.borderColor = UIColor.blueColor().CGColor
+        taxAmount.layer.borderWidth = CGFloat(1.0)
+        taxAmount.layer.borderColor = UIColor.blueColor().CGColor
+        billField.layer.borderWidth = CGFloat(1.0)
+        totalLabek.text = defaults.stringForKey("totalLabek")
+        billField.text = defaults.stringForKey("billField")
+        tipLabel.text = defaults.stringForKey("tipLabel")
+        taxcalculated.text = defaults.stringForKey("taxcalculated")
+        print(tipPercentages)
+                taxAmount.text = defaults.stringForKey("taxAmount")
+        themeGreen = defaults.boolForKey("themeGreen")
+        self.view.backgroundColor = themeGreen == true ? UIColor.greenColor() : UIColor.whiteColor()
+        
+        
+    }
+    
+    
+    
+    var tipPercentages = [0.15,0.18,0.2]
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var themeGreen: Bool = false
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        //let locale = NSLocale.currentLocale()
+        //let currencySymbol = locale.objectForKey(NSLocaleCurrencySymbol) as? String
+        //print("",currencySymbol!)
+        let tax: Double = (taxAmount.text! as NSString).doubleValue
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        print(tipControl.selectedSegmentIndex)
+        let bill: Double = (billField.text! as NSString).doubleValue
+        
+        let tip: Double = bill * tipPercentage
+        //billField.text = currencySymbol! + billField.text!
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        formatter.maximumFractionDigits = 2
+        totalLabek.text = formatter.stringFromNumber(tip + bill+tax)
+        tipLabel.text = formatter.stringFromNumber(tip)
+        print(bill)
+        if bill != 0.0 {
+            taxcalculated.text = String(format: "%.2f",100*tax/bill) + "%"
+        }
+        else{
+            taxcalculated.text = "0.00%"
+        }
+        defaults.setObject(tipPercentages, forKey: "tipPercentages")
+        defaults.setObject(totalLabek.text, forKey: "totalLabek")
+        defaults.setObject(billField.text, forKey: "billField")
+        defaults.setObject(tipLabel.text, forKey: "tipLabel")
+        defaults.setObject(taxcalculated.text, forKey: "taxcalculated")
+        defaults.setObject(taxAmount.text, forKey: "taxAmount")
+        defaults.setBool(themeGreen, forKey: "themeGreen")
+        defaults.synchronize()
+        
+    }
+    
+    @IBAction func settingPressed(sender: AnyObject) {
+        self.performSegueWithIdentifier("Settings" , sender: tipPercentages)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "Settings") {
+            //get a reference to the destination view controller
+            let destinationVC = segue.destinationViewController as! SettingsPage
+            destinationVC.tipPercentages = tipPercentages
+            
+        }
+    }
+
+}
+
